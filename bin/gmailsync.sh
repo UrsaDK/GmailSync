f0da495cccdcc9c70f2da19c5d8595e03d56b3a9
Dmytro Konstantinov <umka.dk@gmail.com>
2013-07-24 18:07:59 +0200

    Initial commit of the legacy code

--- bin/gmailsync.sh
+++ bin/gmailsync.sh
@@ -1,125 +1,385 @@
 #!/bin/bash
+#
+# Master copy of this file is stored in the vcs repository. Make sure that all 
+# stable changes are tested and committed to that repository or they will be 
+# overwritten and lost upon next checkout.
+#
+# Copyright (c) 2011, Dmytro Konstantinov.  All rights reserved.
+#
+# LICENSE: http://opensource.org/licenses/bsd-license.php BSD
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, 
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, 
+#   this list of conditions and the following disclaimer in the documentation 
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the Solar project nor the names of its contributors 
+#   may be used to endorse or promote products derived from this software 
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+#
 
-# Define parameters for the 'From' account 
-USER1=''
-PASS1=''
-HOST1='imap.gmail.com'
-PORT1='993'
-OPT1='--ssl1 --authmech1 LOGIN --split1 100'
+VERSION='1.1.0'
 
-# Define parameters for the 'To' account
-USER2=''
-PASS2=''
-HOST2='imap.gmail.com'
-PORT2='993'
-OPT2='--ssl2 --authmech2 LOGIN --split2 100'
+#
+# CHANGE LOG
+#
+# 1.1   @author Dmytro Konstantinov <umka.dk@gmail.com>
+#
+#       Added a user friendly help message and support for command line 
+#       parameters. Improved error handling: the script now exit with error 
+#       code as per shell standard, all error messages are logged via STDERR.
+#
+# 1.0   @author Dmytro Konstantinov <umka.dk@gmail.com>
+#
+#       Initial release of the code.
+#
 
-# Configure global imapsync parameters
-SYNCMD='imapsync --dry --syncinternaldates --skipsize --useheader "Date" --useheader "Message-ID"'
-LOGFILE='imapsync.log'
-TIMEOUT='10'
+# List default folders that we would like to transfer
+CFG_FOLDERS=( 'INBOX' 'All Mail' 'Bin' 'Drafts' 'Starred' 'Sent Mail' )
+
+# Define location of the log file where to store imapsync messages
+CFG_LOGFILE='/dev/null'
 
 # An email address to which to sent current activity log
-REPORT=''
+CFG_REPORT=''
 
-# List default folders that you would like transfered 
-FOLDERS=( 'INBOX' 'All Mail' 'Bin' 'Drafts' 'Starred' 'Sent Mail' )
+# Configure system timeout (in seconds)
+CFG_TIMEOUT=10
+
+# Define parameters for the origin (from) account 
+CFG_USER1=''
+CFG_PASS1=''
+CFG_HOST1='imap.gmail.com'
+CFG_PORT1='993'
+
+# Define parameters for the destination (to) account
+CFG_USER2=''
+CFG_PASS2=''
+CFG_HOST2='imap.gmail.com'
+CFG_PORT2='993'
 
 #################################################
-## You shouldn't need to modify anything below ##
+##   Do not modify anything below this point   ##
 ##  If you do so, then do it at your own risk  ##
 #################################################
 
-# Escape email address so we can use it in regex
-USER1ESC=`echo ${USER1} | sed 's/@/\\\\@/'`
-USER2ESC=`echo ${USER2} | sed 's/@/\\\\@/'`
+# Configure default imapsync parameters
+BIN_IMAPSYNC=`which imapsync`
+ARG_IMAPSYNC='--dry --syncinternaldates --skipsize --useheader "Date" --useheader "Message-ID"'
 
-# Split transfer by age and add header rewrite rules
-TIMES=( '--minage 1090' '--maxage 1091 --minage 999' '--maxage 1000 --minage 908' '--maxage 909 --minage 817' '--maxage 818 --minage 726' '--maxage 727 --minage 635' '--maxage 636 --minage 544' '--maxage 545 --minage 453' '--maxage 454 --minage 362' '--maxage 363 --minage 271' '--maxage 272 --minage 180' '--maxage 181 --minage 89' '--maxage 90' )
-REGEX=( "s/Delivered-To: ${USER1ESC}/Delivered-To: ${USER2ESC}/gi" "s/<${USER1ESC}>/<${USER2ESC}>/gi" "s/^((To|From|Cc|Bcc):.*)${USER1ESC}(.*)$/\$1${USER2ESC}\$3/gim" 's/Subject:(\s*)\n/Subject: (no--subject)$1\n/ig' 's/Subject: ([Rr][Ee]):(\s*)\n/Subject: $1: (no--subject)$2\n/gi' )
+# Configure imapsync parameters for the origin and destination server
+ARG_IMAPSERV1='--ssl1 --authmech1 LOGIN --split1 100'
+ARG_IMAPSERV2='--ssl2 --authmech2 LOGIN --split2 100'
 
-# Initialise a new logfile
-echo "**** START ****" > $LOGFILE
+# Configure imapsync parameters for staggered mail transfer by message age
+ARGS_STAGGER=( 
+    '--minage 1090'
+    '--maxage 1091 --minage 999'
+    '--maxage 1000 --minage 908'
+    '--maxage 909 --minage 817'
+    '--maxage 818 --minage 726'
+    '--maxage 727 --minage 635'
+    '--maxage 636 --minage 544'
+    '--maxage 545 --minage 453'
+    '--maxage 454 --minage 362'
+    '--maxage 363 --minage 271'
+    '--maxage 272 --minage 180'
+    '--maxage 181 --minage 89'
+    '--maxage 90'
+)
 
-# Override default folders
-if [ "$@" ]; then
-    FOLDERS=( "$@" )
-fi
+# Document available command line options. This is a simple function that 
+# simply outputs script's SYNOPSIS to the user terminal.
+__help() {
+cat << EOF
+Usage:
+    ${0} [options] [arguments]
 
-# Extend option strings with extra params
-OPT1="${OPT1} --host1 '${HOST1}' --port1 ${PORT1} --user1 '${USER1}' --password1 '${PASS1}'"
-OPT2="${OPT2} --host2 '${HOST2}' --port2 ${PORT2} --user2 '${USER2}' --password2 '${PASS2}'"
+        Where [arguments] is an optional, space separated list of folders 
+        which the user would like to synchronise between two accounts. While 
+        [options] could be any of the following:
 
-# Consolidate message rewrtite rules
-for RULE in "${REGEX[@]}"; do
-    RULES="${RULES} --regexmess '${RULE}'"
+        Server connection details:
+
+        -l --log=path           Log output to a file
+        -r --report=email       Email activity log to this address
+        -t --timeout=seconds    Timeout between server operations
+
+        Properties of the origin (from) IMAP account:
+
+        --user1=username        Account username (gmail address)
+        --pass1=password        Account password
+        --host1=hostname        Hostname or IP address
+        --port1=number          Port on which IMAP is listening
+
+        Properties of the destination (to) IMAP account:
+
+        --user2=username        Account username (gmail address)
+        --pass2=password        Account password
+        --host2=hostname        Hostname or IP address
+        --port2=number          Port on which IMAP is listening
+
+        Generic output options:
+
+        -q --quiet              Suppress output of the script
+        -? --help               Display this help message
+        --version               Script and BASH version info
+
+    NOTE: If password to either account is not supplied then it will be 
+    requested from the user during normal script execution.
+
+EOF
+}
+
+# This function is called when the script receives an EXIT signal. It 
+# simulates a common destructor behaviour inside BASH scripts. It allows this 
+# script to free and clean up resources upon termination.
+__exit() {
+    # Send report to an email address
+    if [ ${CFG_REPORT} ]; then
+        say "Emailing result to ${CFG_REPORT}"
+        cat ${CFG_LOGFILE} | mail -s "[GmailSync] Finished importing mail for ${CFG_USER2}" -c "${CFG_REPORT}"
+        rm -f ${CFG_LOGFILE}
+    fi
+}
+
+say() {
+    if [[ -z ${CFG_REPORT} ]]; then
+        echo ${2} "${1}"
+    fi
+}
+
+log() {
+    echo ${2} "${1}" >> $CFG_LOGFILE
+}
+
+# Provide support for command line options.
+#
+# By default BASH does not provide support for long options. However, we can 
+# trick it into doing so by defining '-:' as part of the optspec. This 
+# exploits a non-standard behaviour of the shell which permits the 
+# option-argument to be concatenated to the option, eg: "-f arg" == "-farg"
+while getopts "l:r:t:q?-:" GETOPT; do
+    case ${GETOPT} in
+        -)
+            OPTKEY=`echo ${OPTARG} | sed -e 's/=.*//'`
+            OPTARG=`echo ${OPTARG} | sed -e "s/^${OPTKEY}=\{0,1\}//"`
+
+            # Common fragment of code to test for presence of OPTARG
+            require_OPTARG() {
+                if [[ -z ${OPTARG} ]]; then
+                    echo "${0}: option requires an argument -- ${OPTKEY}" >&2
+                    __help
+                    exit 1
+                fi
+            }
+
+            # Process all long option key and their values
+            case ${OPTKEY} in
+                log)
+                    require_OPTARG
+                    CFG_LOGFILE=${OPTARG}
+                    ;;
+                report)
+                    require_OPTARG
+                    CFG_REPORT=${OPTARG}
+                    ;;
+                timeout)
+                    require_OPTARG
+                    CFG_TIMEOUT=${OPTARG}
+                    ;;
+
+                user1)
+                    require_OPTARG
+                    CFG_USER1=${OPTARG}
+                    ;;
+                pass1)
+                    require_OPTARG
+                    CFG_PASS1=${OPTARG}
+                    ;;
+                host1)
+                    require_OPTARG
+                    CFG_HOST1=${OPTARG}
+                    ;;
+                port1)
+                    require_OPTARG
+                    CFG_PORT1=${OPTARG}
+                    ;;
+
+                user2)
+                    require_OPTARG
+                    CFG_USER2=${OPTARG}
+                    ;;
+                pass2)
+                    require_OPTARG
+                    CFG_PASS2=${OPTARG}
+                    ;;
+                host2)
+                    require_OPTARG
+                    CFG_HOST2=${OPTARG}
+                    ;;
+                port2)
+                    require_OPTARG
+                    CFG_PORT2=${OPTARG}
+                    ;;
+
+                quiet)
+                    exec > /dev/null 2>&1
+                    ;;
+                version)
+                    echo "Shell script $0 version ${VERSION}"
+                    echo `bash --version | head -n 1`
+                    exit
+                    ;;
+                help)
+                    __help
+                    exit
+                    ;;
+                *)
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "${0}: illegal option -- ${OPTKEY}" >&2
+                        __help
+                        exit 1
+                    fi
+                    ;;
+            esac
+            ;;
+
+        l)
+            CFG_LOGFILE=${OPTARG}
+            ;;
+        r)
+            CFG_REPORT=${OPTARG}
+            ;;
+        t)
+            CFG_TIMEOUT=${OPTARG}
+            ;;
+
+        q)
+            exec > /dev/null 2>&1
+            ;;
+        ?)
+            __help
+            exit
+            ;;
+    esac
 done
 
-# Process all folders one by one
-for FOLDER in "${FOLDERS[@]}"; do
-    # Tell user what we are doing
-    if [ -z ${REPORT} ]; then
-        echo "Processing folder: ${FOLDER}"
+# Clear all options and reset the command line argument count
+shift $(( OPTIND -1 ))
+
+# Fail if invalid command line argument was given
+if [[ ${!#} != ${0} ]]; then
+    echo "${0}: illegal argument -- ${!#}" >&2
+    __help
+    exit 1
+fi
+
+# Initialise script destructor
+trap __exit EXIT
+
+#
+# Primary script code
+#
+
+# Verify that the username and password are supplied for both IMAP servers
+for i in 1 2; do
+    user="CFG_USER${i}"
+    pass="CFG_PASS${i}"
+    host="CFG_HOST${i}"
+    port="CFG_PORT${i}"
+
+    # Terminate execution if user, host or port are missing
+    if [[ -z ${!user} ]] || [[ -z ${!host} ]] || [[ -z ${!port} ]]; then
+        echo "${0}: illegal configuration"
+        echo "All of the following must be defined:"
+        echo "    username, password, hostname and port"
     fi
 
+    # Request the user to enter a password if it is missing
+    if [[ ! ${!pass} ]] || [[ -z ${!pass} ]]; then
+        read -rsp "Please enter password for ${!user}:" ${!pass}
+    fi
+done
+
+# Configure imapsync to rewrite old account mail address to the new one
+ESC_USER1=`echo ${CFG_USER1} | sed 's/@/\\\\@/'`
+ESC_USER2=`echo ${CFG_USER2} | sed 's/@/\\\\@/'`
+ARGS_REWRITE=( 
+    "s/Delivered-To: ${ESC_USER1}/Delivered-To: ${ESC_USER2}/gi"
+    "s/<${ESC_USER1}>/<${ESC_USER2}>/gi"
+    "s/^((To|From|Cc|Bcc):.*)${ESC_USER1}(.*)$/\$1${ESC_USER2}\$3/gim"
+    's/Subject:(\s*)\n/Subject: (no--subject)$1\n/ig'
+    's/Subject: ([Rr][Ee]):(\s*)\n/Subject: $1: (no--subject)$2\n/gi'
+)
+
+# Consolidate rewrite rules into a single argument
+ARG_REWRITE=''
+for regex in "${ARGS_REWRITE[@]}"; do
+    ARG_REWRITE="${ARG_REWRITE} --regexmess '${regex}'"
+done
+
+# Consolidate all parameters into a single variable
+ARG_IMAPSERV1="${ARG_IMAPSERV1} --host1 '${CFG_HOST1}' --port1 ${CFG_PORT1} --user1 '${CFG_USER1}' --password1 '${CFG_PASS1}"
+ARG_IMAPSERV2="${ARG_IMAPSERV2} --host2 '${CFG_HOST2}' --port2 ${CFG_PORT2} --user2 '${CFG_USER2}' --password2 '${CFG_PASS2}"
+ARG_IMAPSYNC="${ARG_IMAPSYNC} ${ARG_IMAPSERV1} ${ARG_IMAPSERV2} ${ARG_REWRITE}"
+
+# Process all folders one by one
+for folder in "${CFG_FOLDERS[@]}"; do
+    # Tell user what we are doing
+    say "Processing folder: ${folder}"
+
     # Process message chunks in folders 
-    for TIME in "${TIMES[@]}"; do
-        # Tell user what we are doing
-        if [ -z ${REPORT} ]; then
-            echo "  Retrieving time period : ${TIME}"
-        fi
+    for arg_stagger in "${ARGS_STAGGER[@]}"; do
+        # Tell user & log what we are about to do
+        say "  Retrieving time period : ${arg_stagger}"
+        log ""
+        log "*** ${folder} ${arg_stagger} ***"
+        log ""
 
-        # Tell logfile what we are doing
-        echo "" >> $LOGFILE
-        echo "*** $FOLDER $TIME ***" >> $LOGFILE
-        echo "" >> $LOGFILE
-
-        until eval "${SYNCMD} ${OPT1} ${OPT2} --include '${FOLDER}' ${TIME} ${RULES} >> $LOGFILE 2>&1"; do
+        # Run command until it succeeds
+        until `${BIN_IMAPSYNC} ${ARG_IMAPSYNC} ${arg_stagger}`; do
             # Tell logfile what we are doing
-            echo "" >> $LOGFILE
-            echo "***** NOT COMPLETE - $FOLDER $TIME *****" >> $LOGFILE
-            echo "" >> $LOGFILE
-           
-            # Send action report to email address
-            if [ ${REPORT} ]; then
-                echo "Emailing result to ${REPORT}" >> $LOGFILE
-                tail -100 $LOGFILE | mail -s "Imapsync Restarting for $FOLDER $TIME" "user@domain.com"
-            fi
+            log ""
+            log "***** NOT COMPLETE - ${folder} ${arg_stagger} *****"
+            log ""
 
             # Initialise requested timeout
-            if [ ${TIMEOUT} ]; then
-                MSG="Sleeping for ${TIMEOUT}..."
-
-                # Tell user what is going on
-                if [ -z ${REPORT} ]; then
-                    echo "  ${MSG}"
-                fi
-
-                echo -n ${MSG} >> $LOGFILE
-                sleep ${TIMEOUT}
+            if [ ${CFG_TIMEOUT} ]; then
+                message="Sleeping for ${CFG_TIMEOUT}..."
+                say "  ${message}"
+                log ${message} "-n"
+                sleep ${CFG_TIMEOUT}
             fi
-            
-            echo "Done." >> $LOGFILE
+  
+            log "Done."
         done
 
         # Tell logfile what we are doing
-        echo "" >> $LOGFILE
-        echo "***** COMPLETE - $FOLDER $TIME*****" >> $LOGFILE
-        echo "" >> $LOGFILE
-
-        # Send action report to email address
-        if [ ${REPORT} ]; then
-            echo "Emailing result to ${REPORT}" >> $LOGFILE
-            tail -100 $LOGFILE | mail -s "Imapsync Complete for $FOLDER $TIME" "user@domain.com"
-        fi
+        log ""
+        log "***** COMPLETE - ${folder} ${arg_stagger}*****"
+        log ""
     done
 done
-echo "**** DONE ****" >> $LOGFILE
 
-# Send action report to email address
-if [ ${REPORT} ]; then
-    echo "Emailing result to ${REPORT}" >> $LOGFILE
-    tail -100 $LOGFILE | mail -s "Imapsync Complete" -c "user@domain.com"
-fi
+# Terminate the script
+log "**** DONE ****"
+exit
 
-echo "Finished"
